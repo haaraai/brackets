9beafe7af2542138a911b9b31aaaec0852625653
Yahor Harai <egor.garai@gmail.com>
2022-08-12 00:40:31 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  // let arr = [];
+  // let i = 0;
+  // bracketsConfig.map((el) => {
+  //   arr.push(el.join(""));
+  // })
+  // do {
+  //   let newArr = arr[i];
+  //   if (str.includes(newArr)) {
+  //     str = str.replace(newArr, '');
+  //     i = -1;
+  //   }
+  //   i++;
+  // } while (i < arr.length);
+  // return str.length === 0;
+  
+  for (let i=0; i<bracketsConfig.length; i++){
+     if (str.includes(bracketsConfig[i].join(''))) {
+     str = str.replace(bracketsConfig[i].join(''),'');
+     i=-1;
+   }
+ }
+ return (str === '');
+ 
+
+};
